ae76eb22d6e0ed43af64c15328e7d970eced9753
cosisaxis <kishanelewis2@gmail.com>
2022-07-01 14:44:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is the NFT Minter starter template.
+
+## Getting Started
+
+Clone or download the repo.
+Navigate into the donwloaded/cloned folder and install the dependencies typing the command
+
+```bash
+npm install
+```
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import AuthPage from "../components/AuthPage";
+import UploadForm from "../components/UploadForm";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <AuthPage /> */}
+      <UploadForm />
+    </div>
+  )
+}
