eb1a6f30896d3d4a99c3aab3df25e9b22284481d
cosisaxis <kishanelewis2@gmail.com>
2022-07-01 15:59:31 -0500

    auhtentication-part-1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import { useMoralis } from "react-moralis";
 import AuthPage from "../components/AuthPage";
 import UploadForm from "../components/UploadForm";
 
 export default function Home() {
+  // checks if user is logged in with metamask
+    const {
+      isAuthenticated
+    } = useMoralis()
+
+    if(!isAuthenticated){
+      return(
+        <AuthPage />
+      )
+    }
   return (
     <div>
-      <AuthPage />
-      {/* <UploadForm /> */}
+      <UploadForm />
     </div>
   )
 }
